65b586f62f5aa5c45e08bfe28ced3edd5741862b
BubblyFace <751835145@qq.com>
2020-01-14 22:13:40 +0800

    chore

--- src/index.js
+++ src/index.js
@@ -6,50 +6,42 @@ const options = {
   width: window.innerWidth
 };
 
-class Joystick {
-  constructor(options) {
-    this.widgetArr = [];
-    this.Instance = null;
-    this.init(options);
+init(options);
+
+function init(initOptions) {
+  let root = document.getElementById('root');
+  if (!root) {
+    console.error(`Donnot have root element!`)
   }
 
-  init(initOptions) {
-    let root = document.getElementById('root');
-    if(!root) {
-      console.error(`Donnot have root element!`)
-    }
-
-    canvasInit(initOptions);
-    widgetInit(initOptions);
-  }
-
-  canvasInit(options) {
-    let { root, height, width, background = '#000000' } = options;
-    
-    const canvas = document.createElement('canvas');
-  
-    canvas.id = 'root-canvas';
-    canvas.height = height;
-    canvas.width = width;
-  
-    root.appendChild(canvas);
-    
-    const ctx = canvas.getContext('2d');
-    
-    ctx.fillStyle = background;
-  }
-
-  widgetInit(options) {
-    const widgetDict = require('./config.js');
-  
-    Object.keys(widgetDict).forEach(widgetName => {
-      let widget = new widgetDict[widgetName](options);
-      widgetArr.push({
-        name: widgetName,
-        widget
-      });
-    });
-  }
+  canvasInit(initOptions);
+  widgetInit(initOptions);
 }
 
-module.exports = Joystick.Instance ? Joystick.Instance : new Joystick(options);
+function canvasInit(options) {
+  let { root, height, width, background = '#000000' } = options;
+
+  const canvas = document.createElement('canvas');
+
+  canvas.id = 'root-canvas';
+  canvas.height = height;
+  canvas.width = width;
+
+  root.appendChild(canvas);
+
+  const ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = background;
+}
+
+function widgetInit(options) {
+  const widgetDict = import ('./config.js');
+
+  Object.keys(widgetDict).forEach(widgetName => {
+    let widget = new widgetDict[widgetName](options);
+    widgetArr.push({
+      name: widgetName,
+      widget
+    });
+  });
+}
